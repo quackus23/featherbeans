070d62e7676aba00c33b3c1d061b7565e028c430
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-04-17 19:42:31 +0100

    Update siteConfig

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Jack Oastler', // Site author
+	title: "Jack's cricket blog", // Site title.
+	description: 'Cool blog about all things cricket', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
